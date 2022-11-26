1bda0d4eb94eba5b9958db09b909444a4aa03185
sd990307@gmail.com <songdoyoon@songdoyun-ui-MacBook-Pro.local>
2022-11-26 19:17:33 +0900

    DB added

--- backend/app.js
+++ backend/app.js
@@ -6,8 +6,83 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+const mongoose = require('mongoose');
 var app = express();
+
+const dataarray = [
+  {
+    _id: 0,
+    name: "데자와",
+    link: "https://cdn.imweb.me/thumbnail/20201201/2a7eb5b04812d.jpg",
+    likes: 0
+  },
+  {
+    _id: 1,
+    name: "솔의 눈",
+    link: "https://img.danawa.com/prod_img/500000/945/970/img/1970945_1.jpg?shrink=330:330&_v=20211110135448",
+    likes: 0
+  },
+  {
+    _id: 2,
+    name: "베지밀",
+    link: "https://red.lotteon.com/_m2_/_rsc_/LF1006430/?sref=http://image.lottesuper.co.kr/static-root/fileupload/images/8801033000126_S%20(2).jpg",
+    likes: 0
+  },
+  {
+    _id: 3,
+    name: "두유 검은 콩",
+    link: "https://img.danawa.com/prod_img/500000/438/587/img/1587438_1.jpg?shrink=330:330&_v=20200708133720",
+    likes: 0
+  },
+  {
+    _id: 4,
+    name: "레드불",
+    link: "https://openimage.interpark.com/goods_image_big/2/3/7/5/6549172375_l.jpg",
+    likes: 0
+  },
+  {
+    _id: 5,
+    name: "몬스터 에너지",
+    link: "https://img.danawa.com/prod_img/500000/860/779/img/3779860_1.jpg?shrink=330:330&_v=20220303102552",
+    likes: 0
+  },
+  {
+    _id: 6,
+    name: "몬스터 울트라",
+    link: "https://josundal.com/web/product/big/20200226/9ecfa2c266554d0167a67bd59407d5c2.jpg",
+    likes: 0
+  },
+  {
+    _id: 7,
+    name: "핫 식스",
+    link: "https://img.danawa.com/prod_img/500000/231/686/img/1686231_1.jpg?shrink=330:330&_v=20220902124631",
+    likes: 0
+  },
+  {
+    _id: 8,
+    name: "바나나 우유",
+    link: "https://img.danawa.com/prod_img/500000/518/532/img/9532518_1.jpg?shrink=330:330&_v=20190920150153",
+    likes: 0
+  },
+  {
+    _id: 9,
+    name: "초코에몽",
+    link: "https://img.danawa.com/prod_img/500000/381/608/img/5608381_1.jpg?shrink=330:330&_v=20220617104541",
+    likes: 0
+  },
+  {
+    _id: 10,
+    name: "맥콜",
+    link: "https://img.danawa.com/prod_img/500000/812/904/img/904812_1.jpg?shrink=330:330&_v=20170810085548",
+    likes: 0
+  },
+  {
+    _id: 11,
+    name: "지코",
+    link: "https://img.danawa.com/prod_img/500000/329/182/img/3182329_1.jpg?shrink=330:330&_v=20180427160550",
+    likes: 0
+  }
+];
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -26,6 +101,15 @@ app.use('/users', usersRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
+// Drink.insertMany(dataarray, function(err){
+//   if( err ){
+//     console.log(err);
+//   }
+//   else{
+//     console.log("Successfully updated!");
+//   }
+// })
 
 // error handler
 app.use(function(err, req, res, next) {
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,8 +2,25 @@ var express = require('express');
 var router = express.Router();
 const fs = require('fs')
 const path = require('path');
-const likesData = require('../data/data.json');
-const drinksData = require('../data/drinks.json');
+//const likesData = require('../data/data.json');
+//const drinksData = require('../data/drinks.json');
+let likesData = [];
+let drinksData = [];
+const mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost:27017/drinksDB");
+
+const drinkSchema = new mongoose.Schema({
+  _id: Number,
+  name: String,
+  link: String,
+  likes: Number
+});
+
+const Drink = mongoose.model("Drink", drinkSchema);
+
+
+//db
+
 
 
 const p = path.join(
@@ -23,19 +40,66 @@ router.post('/drink', (req, res, next) => {
   // const updatedData = JSON.stringify(req.body);
   // console.log(updatedData)
   // console.log(req.body)
-  const drinklist = req.body
-  //console.log(drinklist)
-  const freshdrinklistJSON = JSON.stringify(drinklist); //JSON.stringify
+  const drinklist = req.body;
+  console.log(drinklist)
+  //const freshdrinklistJSON = JSON.stringify(drinklist); //JSON.stringify
   //console.log(freshdrinklistJSON);
-  fs.writeFileSync(p,freshdrinklistJSON);//wrote on file system as JSON file
+  //fs.writeFileSync(p,freshdrinklistJSON);//wrote on file system as JSON file
+
+  for( let i = 0; i< 12; i++){
+    Drink.updateOne({_id : i}, {likes: drinklist[i]}, function(err){
+      if(err){
+        console.log(err);
+      } else {
+        console.log("Successfully Updated!");
+      }
+    });
+  }
+
+  
+
   
 });
 
 router.get('/drink', (req, res, next) => {
+  Drink.find(function(err, drinks){
+    if(err){
+      console.log(err);
+    } else {
+  
+      //mongoose.connection.close();
+      //console.log(drinks)
+      drinksData = drinks;
+      //console.log(drinksData);
+      
+      for (let i = 0; i<11; i++){
+        likesData[i] = drinks[i].likes;
+      }
+      //console.log(likesData);
+   
+    }
+  });
   res.send(likesData);
 })
 
 router.get('/getdrinks', (req, res, next)=>{
+  Drink.find(function(err, drinks){
+    if(err){
+      console.log(err);
+    } else {
+  
+      //mongoose.connection.close();
+      //console.log(drinks)
+      drinksData = drinks;
+      //console.log(drinksData);
+      
+      for (let i = 0; i <12; i++){
+        likesData[i] = drinks[i].likes;
+      }
+      //console.log(likesData);
+   
+    }
+  });
   res.send(drinksData);
 })
 
