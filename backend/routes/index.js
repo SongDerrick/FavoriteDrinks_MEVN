f6a1bb57f442218605f410f2011054279a78f04e
sd990307@gmail.com <songdoyoon@songdoyun-ui-MacBook-Pro.local>
2022-12-06 13:24:36 +0900

    for presentation

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,10 +2,10 @@ var express = require('express');
 var router = express.Router();
 const fs = require('fs')
 const path = require('path');
-//const likesData = require('../data/data.json');
-//const drinksData = require('../data/drinks.json');
-let likesData = [];
-let drinksData = [];
+// Requiring various modules
+
+let likesData = []; // array to hold likes data given by user
+let drinksData = []; // array to hold drink data
 const mongoose = require('mongoose');
 
 const drinkSchema = new mongoose.Schema({
@@ -19,7 +19,7 @@ const Drink = mongoose.model("Drink", drinkSchema);
 
 mongoose.connect("mongodb+srv://admin-derrick:test-123@cluster0.ucfbjgj.mongodb.net/?retryWrites=true&w=majority");
 
-//db
+// db configuration => connecting to cloud mongo db with mongoose framework
 
 const p = path.join(
   __dirname,
@@ -28,76 +28,53 @@ const p = path.join(
   'data.json' 
 ); // path configuration
 
-/* GET home page. */
+// GET METHOD '/' -> Getting home page!!
 router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+// POST METHOD '/drink' -> User data posted to server. It updates the DB!!
 router.post('/drink', (req, res, next) => {
 
-  // const updatedData = JSON.stringify(req.body);
-  // console.log(updatedData)
-  // console.log(req.body)
-  const drinklist = req.body;
-  //console.log(drinklist)
-  //const freshdrinklistJSON = JSON.stringify(drinklist); //JSON.stringify
-  //console.log(freshdrinklistJSON);
-  //fs.writeFileSync(p,freshdrinklistJSON);//wrote on file system as JSON file
-
-  for( let i = 0; i< 12; i++){
+  const drinklist = req.body; // saves user request(user data) to given array
+  for( let i = 0; i< 12; i++){ // for given 12 drinks update its likes
     Drink.updateOne({_id : i}, {likes: drinklist[i]}, function(err){
       if(err){
         console.log(err);
       } else {
-       //console.log("Successfully Updated!");
-       //mongoose.connection.close();
+
       }
     });
-  }
-
-  
-
-  
+  }  
 });
 
+// GET METHOD '/drink' -> send likes data for each drinks, get likes data from the DB!!
 router.get('/drink', (req, res, next) => {
   Drink.find(function(err, drinks){
     if(err){
       console.log(err);
     } else {
-  
-      
-      //console.log(drinks)
       drinksData = drinks;
-      //console.log(drinksData);
-      
-      for (let i = 0; i<11; i++){
+      for (let i = 0; i<12; i++){
         likesData[i] = drinks[i].likes;
       }
-      //console.log(likesData);
-      //mongoose.connection.close();
    
     }
   });
-  res.send(likesData);
+  res.send(likesData); // send array which contains likes for each drinks
 })
 
+// GET METHOD '/getdrinks' -> send drink data for each drinks, get drink data from the DB!!
 router.get('/getdrinks', (req, res, next)=>{
-  //mongoose.connect("mongodb://localhost:27017/drinksDB");
   Drink.find(function(err, drinks){
     if(err){
       console.log(err);
     } else {
   
-      //mongoose.connection.close();
-      //console.log(drinks)
       drinksData = drinks;
-      //console.log(drinksData);
-      
       for (let i = 0; i <12; i++){
         likesData[i] = drinks[i].likes;
       }
-      //console.log(likesData);
    
     }
   });
